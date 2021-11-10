0165991f2bd0bb64470bb8178bf61838e622a79e
Quynh Anh Pham <qap.pham@gmail.com>
2021-11-10 16:55:14 +0100

    Exercise Yelp finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,11 @@ Restaurant.destroy_all
 puts 'Creating 5 fake restaurants...'
 5.times do
   restaurant = Restaurant.new(
-    name: Faker::Company.name,
+    name: Faker::Beer.name,
     address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
     phone_number: Faker::PhoneNumber.phone_number,
-    category: Faker::Food.ethnic_category
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
   )
-  restaurant.save!
+  restaurant.save
 end
 puts 'Finished!'
